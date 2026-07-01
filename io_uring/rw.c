2d1de0fa37a1f6098eaa6003380c954a42a3ae69
Amon Ott <ao@rsbac.org>
2026-07-01 11:15:00 +0200

    Intercept direct read and write in io_uring/rw.c with rsbac_handle_rw_req()

--- io_uring/rw.c
+++ io_uring/rw.c
@@ -13,6 +13,8 @@
 #include <linux/io_uring/cmd.h>
 #include <linux/indirect_call_wrapper.h>
 
+#include <rsbac/hooks.h>
+
 #include <uapi/linux/io_uring.h>
 
 #include "filetable.h"
@@ -839,13 +841,32 @@ static bool io_rw_should_retry(struct io_kiocb *req)
 static inline int io_iter_do_read(struct io_rw *rw, struct iov_iter *iter)
 {
 	struct file *file = rw->kiocb.ki_filp;
+	int err;
+
+#ifdef CONFIG_RSBAC_RW
+	struct rsbac_rw_req rsbac_rw_req_obj;
+#endif
+
+#ifdef CONFIG_RSBAC_RW
+	rsbac_rw_req_obj.rsbac_target = T_NONE;
+	rsbac_rw_req_obj.rsbac_request = R_READ;
+	if (!rsbac_handle_rw_req(file, &rsbac_rw_req_obj))
+		return -EPERM;
+#endif
 
 	if (likely(file->f_op->read_iter))
-		return file->f_op->read_iter(&rw->kiocb, iter);
+		err = file->f_op->read_iter(&rw->kiocb, iter);
 	else if (file->f_op->read)
-		return loop_rw_iter(READ, rw, iter);
+		err = loop_rw_iter(READ, rw, iter);
 	else
-		return -EINVAL;
+		err = -EINVAL;
+
+#ifdef CONFIG_RSBAC_RW
+	if (err > 0)
+		rsbac_handle_rw_up(&rsbac_rw_req_obj);
+#endif
+
+	return err;
 }
 
 static bool need_complete_io(struct io_kiocb *req)
@@ -1142,6 +1163,10 @@ int io_write(struct io_kiocb *req, unsigned int issue_flags)
 	ssize_t ret, ret2;
 	loff_t *ppos;
 
+#ifdef CONFIG_RSBAC_RW
+	struct rsbac_rw_req rsbac_rw_req_obj;
+#endif
+
 	if (req->flags & REQ_F_IMPORT_BUFFER) {
 		ret = io_rw_import_reg_vec(req, io, ITER_SOURCE, issue_flags);
 		if (unlikely(ret))
@@ -1176,6 +1201,13 @@ int io_write(struct io_kiocb *req, unsigned int issue_flags)
 	if (unlikely(ret))
 		return ret;
 
+#ifdef CONFIG_RSBAC_RW
+	rsbac_rw_req_obj.rsbac_target = T_NONE;
+	rsbac_rw_req_obj.rsbac_request = R_WRITE;
+	if (!rsbac_handle_rw_req(req->file, &rsbac_rw_req_obj))
+		return -EPERM;
+#endif
+
 	if (unlikely(!io_kiocb_start_write(req, kiocb)))
 		return -EAGAIN;
 	kiocb->ki_flags |= IOCB_WRITE;
@@ -1186,6 +1218,11 @@ int io_write(struct io_kiocb *req, unsigned int issue_flags)
 		ret2 = loop_rw_iter(WRITE, rw, &io->iter);
 	else
 		ret2 = -EINVAL;
+
+#ifdef CONFIG_RSBAC_RW
+	if (ret2 > 0)
+		rsbac_handle_rw_up(&rsbac_rw_req_obj);
+#endif
 
 	/*
 	 * Raw bdev writes will return -EOPNOTSUPP for IOCB_NOWAIT. Just
