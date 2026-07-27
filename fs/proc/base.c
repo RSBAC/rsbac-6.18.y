640567835ec0569d2cdb6d8b08b30ad94d221631
Amon Ott <ao@rsbac.org>
2026-07-27 09:58:17 +0200

    Fix merge error in fs/proc/base.c:proc_root_link()

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -211,6 +211,27 @@ static int get_task_root(struct task_struct *task, struct path *root)
 {
 	int result = -ENOENT;
 
+#ifdef CONFIG_RSBAC
+	union rsbac_target_id_t rsbac_target_id;
+	union rsbac_attribute_value_t rsbac_attribute_value;
+
+	rsbac_pr_debug(aef, "calling ADF\n");
+	rsbac_target_id.process = get_task_pid(task, PIDTYPE_PID);
+	if (rsbac_target_id.process) {
+		rsbac_attribute_value.dummy = 0;
+		if (!rsbac_adf_request(R_GET_STATUS_DATA,
+					task_pid(current),
+					T_PROCESS,
+					rsbac_target_id,
+					A_none,
+					rsbac_attribute_value)) {
+			put_pid(rsbac_target_id.process);
+			return -EPERM;
+		}
+		put_pid(rsbac_target_id.process);
+	}
+#endif
+
 	task_lock(task);
 	if (task->fs) {
 		get_fs_root(task->fs, root);
@@ -261,29 +282,6 @@ static int proc_root_link(struct dentry *dentry, struct path *path,
 			  struct task_struct *task)
 {
 	return get_task_root(task, path);
-
-#ifdef CONFIG_RSBAC
-		union rsbac_target_id_t rsbac_target_id;
-		union rsbac_attribute_value_t rsbac_attribute_value;
-
-		rsbac_pr_debug(aef, "calling ADF\n");
-		rsbac_target_id.process = get_task_pid(task, PIDTYPE_PID);
-		if (rsbac_target_id.process) {
-			rsbac_attribute_value.dummy = 0;
-			if (!rsbac_adf_request(R_GET_STATUS_DATA,
-						task_pid(current),
-						T_PROCESS,
-						rsbac_target_id,
-						A_none,
-						rsbac_attribute_value)) {
-				put_pid(rsbac_target_id.process);
-				put_task_struct(task);
-				return -EPERM;
-			}
-			put_pid(rsbac_target_id.process);
-		}
-#endif
-
 }
 
 /*
